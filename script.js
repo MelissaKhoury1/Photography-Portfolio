ebe73365eaf69eff0ba066b02d8c65508318f6b6
m <melissakhoury00@gmail.com>
2025-06-18 19:02:37 +0300

    Initial commit of photography project

---
+++ script.js
@@ -0,0 +1,19 @@
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+function closeFullImg() {
+    fullImgBox.style.display = "none";
+}
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')   
+})
